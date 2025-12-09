9d2997a9553b9618817567497a5bf4300d03debb
Juan-Zamora3 <juan-zamorag311@outlook.es>
2025-12-09 08:47:49 -0700

    xdd

--- frontend/src/modulos/administradorEventos/paginas/LayoutAdminEventos.tsx
+++ frontend/src/modulos/administradorEventos/paginas/LayoutAdminEventos.tsx
@@ -3,13 +3,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import HeaderAdminEventos from "../componentes/comunes/HeaderAdminEventos";
 
-
 const LayoutAdminEventos: React.FC = () => {
-
   return (
     <div className="h-screen w-full bg-gradient-to-b from-[#192D69] to-[#6581D6] flex flex-col">
+      {/* Barra superior con usuario/correo/cerrar sesión */}
       <HeaderAdminEventos />
 
+      {/* Contenido: igual que tu diseño original, sin px-8/py-6 aquí */}
       <main className="flex-1 min-h-0 overflow-y-auto">
         <Outlet />
       </main>
